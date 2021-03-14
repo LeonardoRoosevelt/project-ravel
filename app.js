50a2e790456b746e455ca52f4411739b9978268e
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-03-14 13:22:17 +0800

    feat: routes init

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const routes = require('./routes')
+
+routes(app)
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
