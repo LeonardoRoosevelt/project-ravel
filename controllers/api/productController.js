86654060b2c4d0e2e462722b2f0866c7ba6f89d8
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-03-15 16:30:09 +0800

    feat: add POST /api/product

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -1,5 +1,29 @@
+const db = require('../../models')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const Product = db.Product
+
 const productController = {
-  createProduct: () => {},
+  createProduct: (req, res, next) => {
+    const { id, name, quantity } = req.body
+    if (!id || !name || !quantity) {
+      return res.status(400).json({ message: 'Please enter the value.' })
+    }
+    if (quantity < 0) {
+      return res.status(400).json({ message: "The quantity can't be less than zero." })
+    }
+    Product.findOrCreate({
+      where: { [Op.or]: [{ id: { [Op.eq]: id } }, { name: { [Op.eq]: name } }] },
+      defaults: { id, name, quantity }
+    })
+      .then(([product, created]) => {
+        if (!created) {
+          return res.status(400).json({ message: 'The value of id or name already exists.' })
+        }
+        return res.json({ message: 'Product create successfully!', product })
+      })
+      .catch(next)
+  },
   getProduct: () => {},
   updateProduct: () => {},
   deleteProduct: () => {},
