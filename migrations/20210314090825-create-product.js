6586e7e2e5da4b3437b9bedc1e47bab3a29dc5b6
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-03-15 16:26:48 +0800

    feat: add product model

---
+++ migrations/20210314090825-create-product.js
@@ -0,0 +1,31 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products')
+  }
+}
