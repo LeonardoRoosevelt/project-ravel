9d0f7d4b90d57f1d3ccdf5909bef57e5a4526198
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-03-15 23:26:05 +0800

    feat: add POST /api/price

--- controllers/api/priceController.js
+++ controllers/api/priceController.js
@@ -1,5 +1,43 @@
+const db = require('../../models')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const Product = db.Product
+const Price = db.Price
+
 const priceController = {
-  createPrice: () => {},
+  createPrice: (req, res, next) => {
+    const { id, productId, type, price } = req.body
+    if (!id || !productId || !type || !price) {
+      return res.status(400).json({ message: 'Please enter all values.' })
+    }
+    return Product.findByPk(productId)
+      .then((product) => {
+        if (!product) {
+          return res.status(400).json({ message: 'This product is not exist.' })
+        }
+        if (type !== 'Gold' && type !== 'Silver' && type !== 'Copper') {
+          return res.status(400).json({ message: 'This type is wrong. Please enter Gold, Silver or Copper' })
+        }
+        return Price.findOrCreate({
+          where: {
+            [Op.or]: [
+              { id: { [Op.eq]: id } },
+              { [Op.and]: [{ productId: { [Op.eq]: productId } }, { type: { [Op.eq]: type } }] }
+            ]
+          },
+          defaults: { id, productId, type, price }
+        }).then(([price, created]) => {
+          if (!created) {
+            if (id === price.id) {
+              res.status(400).json({ message: 'The id  already exists.' })
+            }
+            return res.status(400).json({ message: 'The type of the product price already exists.' })
+          }
+          return res.json({ message: 'Price create successfully!', price })
+        })
+      })
+      .catch(next)
+  },
   getPrice: () => {},
   updatePrice: () => {},
   deletePrice: () => {}
